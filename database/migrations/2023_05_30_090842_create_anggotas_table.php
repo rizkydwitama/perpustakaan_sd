3e6b53a713a05d8caffa60b786cd48b923c32a25
Tiara <tiarafirdausa94@gmail.com>
2023-06-03 02:47:50 +0700

    db anggota buku

---
+++ database/migrations/2023_05_30_090842_create_anggotas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_anggota');
+            $table->string('nomor_induk_anggota');
+            $table->string('kelas');
+            $table->integer('jumlah_pinjam');
+            $table->boolean('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anggotas');
+    }
+};
